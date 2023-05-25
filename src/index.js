43e91d8aa18c1a5a4892aa007d0a1add871f4780
RicardoUy <rvuy62@yahoo.com>
2023-05-25 15:33:40 -0500

    added to html, css, index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-const ini = () => {
-    const inputForm = document.querySelector("form1");
+const init = () => {
+    const inputForm = document.querySelector("form");
     inputForm.addEventListener("submit", (event)  => {
       event.preventDefault();
      
@@ -11,19 +11,19 @@ const ini = () => {
      .then((response) => response.json())
      .then((data) => {
       //const brand = document.querySelector("section#TshirtDetails h4");
-      //const neckDesign = document.querySelector("section#TshirtDetails h4");
-     // const color = document.querySelector("section#TshirtDetails h4");
-      //const size = document.querySelector("section#TshirtDetails h4");
-      const price = document.querySelector("section#TshirtDetails h4");
-      const inventory = document.querySelector("section#TshirtDetails p");
-     // brand.innerText = data.brand;
-      //neckDesign.innerText = data.neckDesign;
-     // color.innerText = data.color;
-      //size.innerText = data.size;
+      const neckDesign = document.querySelector("section#TshirtDetails p1");
+      const color = document.querySelector("section#TshirtDetails p2");
+     const size = document.querySelector("section#TshirtDetails p3");
+     const price = document.querySelector("section#TshirtDetails p4");
+      const inventory = document.querySelector("section#TshirtDetails p5");
+      //brand.innerText = data.brand;
+      neckDesign.innerText = data.neckDesign;
+      color.innerText = data.color;
+      size.innerText = data.size;
       price.innerText = data.price;
       inventory.innerText = data.inventory;
      });
     });
   }
   
-  document.addEventListener('DOMContentLoaded', ini);
+  document.addEventListener('DOMContentLoaded', init);
