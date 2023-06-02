ad5bd9a0ba7b42e510dc09ab15cc160b33fa08ed
RicardoUy <rvuy62@yahoo.com>
2023-06-01 21:05:57 -0500

    added to index.js and html

--- src/index.js
+++ src/index.js
@@ -27,3 +27,81 @@ const init = () => {
   }
   
   document.addEventListener('DOMContentLoaded', init);
+  let processedData = [];
+  const init1 = () => {
+    const inputForm = document.querySelector("form");
+    inputForm.addEventListener("submit", (event)  => {
+      event.preventDefault();
+     
+     const input = document.querySelector("input#EnterProductNumber");
+  
+     console.log(input.value);
+     
+     fetch(`http://localhost:3000/tshirt/${input.value}`)
+     .then((response) => response.json())
+     .then((data) => {
+       
+      let totalAmount = 0;
+      let numItems = 0;
+       numItems = document.querySelector("section#TshirtDetail p1");
+       totalAmount = document.querySelector("section#TshirtDetail p2");
+      //const pricePtax = document.querySelector("section#TshirtDetail p3");
+      //const updatedInv = document.querySelector("section#TshirtDetail p4");
+      price.innerText = data.price;
+      processedData.push(price.innerText);
+      function findTotalAmount() {
+        
+           for (const item of processedData) {
+          totalAmount += item;
+          numItems += 1;
+          return totalAmount;
+                 
+           }
+      }
+      //pricePtax = price.innerText + price.innerText* 0.1;
+      //return pricePtax;
+     // inventory.innerText = data.inventory;
+     // updatedInv = inventory.innerText - 1;
+     // return updatedInv;
+      
+     // price.innerText = data.price;
+     //pricePtax = data.price + data.price* 0.1;
+     // return pricePtax;
+     // inventory.innerText = data.inventory;
+     //updatedInv = data.inventory - 1;
+      //return updatedInv;
+
+     });
+    });
+  } 
+document.addEventListener('DOMContentLoaded', init1);
+document.getElementById("myBtn").addEventListener("click", addToCart());
+
+function addToCart() {
+ document.getElementById("demon").innerHTML = displayCart();
+}
+function displayCart() {
+  let totalAmount = 0;
+     for (const item of processedData) {
+    totalAmount += item;
+    return totalAmount;
+     }
+}
+
+  document.getElementById("myBtn1").addEventListener("click", displayCheckOut);
+
+ function displayCheckOut() {
+  document.getElementById("demo").innerHTML = checkOut();
+ }
+ function checkOut() {
+
+ }
+
+ document.getElementById("myBtn2").addEventListener("click", displayTy);
+
+ function displayTy() {
+  document.getElementById("demo").innerHTML = thankYou();
+ }
+ function thankYou(){
+  console.log( "Thank You For Shopping At RICARDO'S KAMISETA");
+ }
